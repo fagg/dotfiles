439bd854f4dc880c9f6532907f6d477d65f85c73
Ashton Fagg <ashton@fagg.id.au>
2015-08-16 02:57:57 -0400

    Updated org mode settings for more awesome

--- emacs/.emacs.d/settings/org-settings.el
+++ emacs/.emacs.d/settings/org-settings.el
@@ -1,4 +1,15 @@
 (setq org-directory "~/Dropbox/org")
 (setq org-agenda-files '("~/Dropbox/org/"))
+(setq org-log-done 'time)
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "STARTED(s)" "DELAYED(w)" "|" "DONE(d)" "CANCELLED(c)")))
+
+(setq org-todo-keyword-faces
+      '(("TODO" . (:foreground "orange" :weight bold))
+        ("STARTED" . (:foreground "light blue" :weight bold))
+        ("DONE" . (:foreground "green" :weight bold))
+        ("CANCELLED" . (:foreground "red" :weight bold))
+        ("DELAYED" . (:foreground "yellow" :weight bold))))
 
 (provide 'org-settings)
